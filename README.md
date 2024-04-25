e3f04aed4a66ee9b0cd08c88b61dbffca4504c7a
Nnisik <nisik0120@gmail.com>
2024-04-26 01:09:20 +0300

    version 1.0 complete

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## Functionality
 * 
 
+![Svelte](https://img.shields.io/badge/svelte-%23f1413d.svg?style=for-the-badge&logo=svelte&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+
 ### ToDo
 * switch "inputValue" and "outputValue" when changing currencies
 
--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,30 @@
 <script lang="ts">
   import "./style/style.css";
 
-  const intervals = setInterval(() => {
-    outputValue = exchangesRates.conversion_rates[selectOutputCurrency];
-  }, 500);
+  function calculateInputValue() {
+    inputValue = (outputValue / exchangesRates.conversion_rates[selectInputCurrency]).toFixed(3);
+  }
   
-  // function calculateFinalResult() {
-  //   outputValue = inputValue * exchangesRates.conversion_rates[selectOutputCurrency]
-  // }
+  function calculateOutputValue() {
+    outputValue = (exchangesRates.conversion_rates[selectOutputCurrency] * inputValue).toFixed(3);
+  }
+
   function changeInputCurrancy() {
-    fetchAPI();
+    calculateInputValue();
+  }
+
+  function changeOutputCurrancy() {
+    outputValue = exchangesRates.conversion_rates[selectOutputCurrency];
+    calculateOutputValue();
   }
 
   function fetchAPI() {
-    fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${selectInputCurrency}`,{method: "GET"})
-      .then(response => response.json())
-      .then(data => {
-        // document.querySelector("#output")?.textContent = data.
+    fetch(
+      `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${selectInputCurrency}`,
+      { method: "GET" }
+    )
+      .then((response) => response.json())
+      .then((data) => {
         outputValue = data.conversion_rates[selectOutputCurrency];
         exchangesRates = data;
       });
@@ -26,22 +34,14 @@
   // api key
   const API_KEY = "be3fe510a77785002265eff3";
   // current exchange rates
-  let exchangesRates: { conversion_rates: { [x: string]: number; }; };
-  let selectInputCurrency = 'USD';
-  let selectOutputCurrency = 'USD';
+  let exchangesRates: { conversion_rates: { [x: string]: any; }; };
+  let selectInputCurrency = "USD";
+  let selectOutputCurrency = "EUR";
   // $: selectOutputCurrency, changeOutputCurrency();
-  const currencyOptions = [
-    'USD',
-    'EUR',
-    'CHF',
-    'GBP',
-    'CNY',
-    'JPY',
-    'RUB'
-  ];
-  let inputValue: any;
+  const currencyOptions = ["USD", "EUR", "CHF", "GBP", "CNY", "JPY", "RUB"];
+  let inputValue: any = 1;
   let outputValue: any;
-
+  
   fetchAPI();
 
 </script>
@@ -51,16 +51,39 @@
     <h1 class="converter-header">Convert currency</h1>
     <section class="converter-fields">
       <div class="currency-container" id="search">
-        <input type="number" class="input" placeholder="1" id="input" bind:value={inputValue} />
-        <select class="currensy-celect-btn" id="input-currency" bind:value={selectInputCurrency} on:change={changeInputCurrancy} >
+        <input
+          type="number"
+          class="input"
+          placeholder="1"
+          id="input"
+          bind:value={inputValue}
+          on:change={calculateOutputValue}
+        />
+        <select
+          class="currensy-celect-btn"
+          id="input-currency"
+          bind:value={selectInputCurrency}
+          on:change={changeInputCurrancy}
+        >
           {#each currencyOptions as currency}
             <option {currency}>{currency}</option>
           {/each}
         </select>
       </div>
       <div class="currency-container" id="result">
-        <p class="input" id="output">{outputValue}</p>
-        <select class="currensy-celect-btn" id="result-currency" bind:value={selectOutputCurrency} >
+        <input
+          type="number"
+          class="input"
+          id="output"
+          bind:value={outputValue}
+          on:change={calculateInputValue}
+        />
+        <select
+          class="currensy-celect-btn"
+          id="result-currency"
+          bind:value={selectOutputCurrency}
+          on:change={changeOutputCurrancy}
+        >
           {#each currencyOptions as currency}
             <option {currency}>{currency}</option>
           {/each}
